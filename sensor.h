04f662b932a88100cd79b1486fa3bb2d6706b4b9
camistolo <40144517+camistolo@users.noreply.github.com>
2018-09-05 17:40:25 -0300

    Update sensor.h

--- sensor.h
+++ sensor.h
@@ -4,55 +4,22 @@
 #include <iostream>
 
 class sensor {
-	double *sensor;
+	double *sensor;	//Un vector con los valores de un sensor
+	int longitud; //Longitud del vector sensor (variable)
 public:
-	sensor(double,double,double,double);	//Crea un vector sensor
-	double posicion(int);	//Encuentra el valor en la posicion int
-	double promedio(int, int);	//Hace el promedio entre la posicion int y la posicion int
-	double maximo(int, int);	//Hace el maximo entre la posicion int y la posicion int
-	double minimo(int, int);	//Hace el maximo entre la posicion int y la posicion int
-	double cantidad(int, int);
+	double promedio(&sensor,int, int);	//Hace el promedio entre la posicion int y la posicion int
+	double maximo(&sensor,int, int);	//Hace el maximo entre la posicion int y la posicion int
+	double minimo(&sensor,int, int);	//Hace el minimo entre la posicion int y la posicion int
+	double cantidad(&sensor,int, int);	//Calcula la cantidad de valores que se usaron para las operaciones anteriores
 
-
-	friend std::istream &operator>>(std::istream &, sensor &*);	//Lee el archivo y lo convierte en una matriz
+	friend std::istream &operator>>(std::istream &, sensor &**);	//Lee el archivo y lo convierte en una matriz
 	friend std::ostream &operator<<(std::ostream &, const sensor &*);
+	//Chequear si la entrada es correcta
+	//Chequear si la consulta es correcta
+	//Chequear que el sensor sea existente
 
-
-}
-
-
-class complejo {
-	double re_, im_;
-public:
-	complejo();
-	complejo(double);
-	complejo(double, double);
-	complejo(const complejo &);
-	complejo const &operator=(complejo const &);
-	complejo const &operator*=(complejo const &);
-	complejo const &operator+=(complejo const &);
-	complejo const &operator-=(complejo const &);
-	~complejo();
-
-	double re() const;
-	double im() const;
-	double abs() const;
-	double abs2() const;
-	complejo const &conjugar();
-	complejo const conjugado() const;
-	bool zero() const;
-
-	friend complejo const operator+(complejo const &, complejo const &);
-	friend complejo const operator-(complejo const &, complejo const &);
-	friend complejo const operator*(complejo const &, complejo const &);
-	friend complejo const operator/(complejo const &, complejo const &);
-	friend complejo const operator/(complejo const &, double);
-
-	friend bool operator==(complejo const &, double);
-	friend bool operator==(complejo const &, complejo const &);
-
-	friend std::ostream &operator<<(std::ostream &, const complejo &);
-	friend std::istream &operator>>(std::istream &, complejo &);
 };
+
+
 
 #endif
