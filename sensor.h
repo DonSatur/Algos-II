7d103f5529501c603f102942d4ed99a396d0d6db
camistolo <40144517+camistolo@users.noreply.github.com>
2018-09-15 15:41:46 -0300

    Update sensor.h

--- sensor.h
+++ sensor.h
@@ -1,22 +1,29 @@
-#ifndef _SENSOR_H_INCLUDED_
 #define _SENSOR_H_INCLUDED_
 
 #include <iostream>
+using namespace std
 
 class sensor {
-	double *sensor;	//Un vector con los valores de un sensor
-	int longitud; //Longitud del vector sensor (variable)
+	string name;	//El nombre del sensor
+	Array <double> values;	//Un vector con los valores del sensor
+	
 public:
-	double promedio(&sensor,int, int);	//Hace el promedio entre la posicion int y la posicion int
-	double maximo(&sensor,int, int);	//Hace el maximo entre la posicion int y la posicion int
-	double minimo(&sensor,int, int);	//Hace el minimo entre la posicion int y la posicion int
-	double cantidad(&sensor,int, int);	//Calcula la cantidad de valores que se usaron para las operaciones anteriores
+	sensor();
+	sensor(size_t n);
+	sensor( const sensor & ); 
+	~sensor( );
+	sensor&	operator=( const sensor & ); 
+	bool 		operator==( const sensor & ) const; 
+	bool 		operator!=( const sensor & ) const; 
+	sensor &		operator[ ]( int );
+	sensor const &	operator[ ]( int ) const;
 
-	friend std::istream &operator>>(std::istream &, sensor &**);	//Lee el archivo y lo convierte en una matriz
-	friend std::ostream &operator<<(std::ostream &, const sensor &*);
-	//Chequear si la entrada es correcta
-	//Chequear si la consulta es correcta
-	//Chequear que el sensor sea existente
+	double	mean(&sensor,int, int) const;	//Hace el promedio entre la posicion int y la posicion int
+	double	max(&sensor,int, int) const;	//Hace el maximo entre la posicion int y la posicion int
+	double	min(&sensor,int, int) const;	//Hace el minimo entre la posicion int y la posicion int
+	double	subsize(&sensor,int, int) const;	//Calcula la cantidad de valores que se usaron para las operaciones anteriores
+
+	friend std::istream& operator>>(std::istream&,sensor&);
 
 };
 
