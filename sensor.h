8a6fa36ed76d5b42fe294bc145613fa5390e3bbb
Ignacio Davila Gallesio <nachidavila@gmail.com>
2018-09-16 17:47:59 -0300

    Update sensor.h

--- sensor.h
+++ sensor.h
@@ -1,9 +1,15 @@
 #ifndef _SENSOR_H_INCLUDED_
 #define _SENSOR_H_INCLUDED_
 
+#include <cmath>
+#include <cassert>
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <stdio.h>
 #include "Array.h"
-using namespace std
+
+using namespace std;
 
 class sensor {
 	string ID;	//El nombre del sensor
@@ -11,26 +17,113 @@ class sensor {
 	
 public:
 	sensor();
-	sensor(size_t n);
 	sensor(string a);
 	sensor(string a, size_t n);
-	sensor( const sensor & ); 
+	sensor( const sensor & S); 
 	~sensor( );
-	sensor&		operator=( const sensor & ); 
-	bool 		operator==( const sensor & ) const; 
-	bool 		operator!=( const sensor & ) const; 
-	sensor &		operator[ ]( int );
-	sensor const &	operator[ ]( int ) const;
-
-	double	mean(&sensor,int, int) const;	//Hace el promedio entre la posicion int y la posicion int
-	double	max(&sensor,int, int) const;	//Hace el maximo entre la posicion int y la posicion int
-	double	min(&sensor,int, int) const;	//Hace el minimo entre la posicion int y la posicion int
-	double	subsize(&sensor,int, int) const;	//Calcula la cantidad de valores que se usaron para las operaciones anteriores
-
-	//friend std::istream& operator>>(std::istream&,sensor&);
+	sensor&		operator=( const sensor & S); 
+	bool 		operator==( const sensor & S) const; 
+	bool 		operator!=( const sensor & S) const; 
+	double &		operator[ ]( int pos);
+	double const &	operator[ ]( int pos) const;
 
 };
 
+
+sensor::sensor()
+{
+	values=0;
+	ID='\0';
+}
+
+sensor::sensor(string a)
+{
+	values=0;
+	ID=a;
+}
+ 
+sensor::sensor(string a, size_t n)
+{
+	Array <double> aux(n);
+	values = aux;
+	ID=a;
+}
+
+sensor::sensor(const sensor & S)
+{	
+	values=S.values;
+	ID=S.ID;
+}
+
+sensor::~sensor()
+{
+	values.~Array();
+}
+
+sensor&
+sensor::operator=( const sensor & S)
+{
+	if ( S.ID == this->ID && S.values==this->values) 
+	{
+		return *this;
+	}
+	// Después, cambiamos el tamaño del arreglo si es necesario y procedemos a copiar
+	if(S.ID != this->ID)
+	{
+		ID=S.ID;
+	}
+
+	if (S.values != this->values)
+	{
+		values=S.values;
+	}
+
+	return *this;
+
+} 
+
+ 
+bool
+sensor::operator==( const sensor & S) const
+{
+	if ( ID != S.ID)
+		return false; 
+	else{
+			if (values != S.values){
+				return false;
+			}
+		}
+
+       	return true;
+}
+ 
+bool
+sensor::operator!=( const sensor & S) const
+{
+	if ( ID == S.ID)
+		return false; 
+	else{
+			if (values == S.values){
+				return false;
+			}
+		}
+
+       	return true;
+}
+
+
+double &
+sensor::operator[ ](int pos)
+{
+	return this->values[pos];
+}
+
+
+double const &
+sensor::operator[ ](int pos) const
+{
+	return this->values[pos];
+}
 
 
 #endif
